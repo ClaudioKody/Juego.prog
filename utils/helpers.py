038bb1fb04130b7263ba24dc7edd9ba4064aa8fb
Eneles070 <jilenequin133@gmail.com>
2026-08-05 18:57:59 -0300

    codigo de helpers finalizado

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,8 +1,11 @@
-import  pygame 
-def  load_image ( path ,  scale = 1 ): 
-image  =  pygame . image . load ( path ). convert_alpha () 
-if  scale  !=  1 : 
-size  =  image . get_size () 
-image  =  pygame . transform . scale ( image , ( int ( size [ 0 ]  *  scale ), 
-int ( size [ 1 ]  *  scale ))) 
-return  image
+import pygame 
+
+def load_image(path, scale=1):
+    image = pygame.image.load(path).convert_alpha()
+    if scale != 1:
+        size = image.get_size()
+        image = pygame.transform.scale(image, (int(size[0] * scale), int(size[1] * scale)))
+    return image
+
+def check_collision(rect1, rect2):
+    return rect1.colliderect(rect2)
