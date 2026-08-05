b1678639ffd25c62a9e316cbe1d8aa2f87a03b96
Eneles070 <jilenequin133@gmail.com>
2026-08-05 18:44:48 -0300

    score_manager finalizado y corregido

--- repository/managers/score_manager.py
+++ repository/managers/score_manager.py
@@ -1,7 +1,11 @@
-from  repository . database . score_repository  import  ScoreRepository
+from repository.database.score_repository import ScoreRepository
 
-class  ScoreManager : 
-    def  __init__ ( self ): 
-        self . score_repository  =  ScoreRepository () 
-    def  add_score ( self ,  player_name ,  score ): 
-        self . score_repository . save_score ( player_name ,  score)
+class ScoreManager:
+    def __init__(self):
+        self.score_repository = ScoreRepository()
+
+    def add_score(self, player_name, score):
+        self.score_repository.save_score(player_name, score)
+
+    def get_top_scores(self, limit=10):
+        return self.score_repository.get_high_scores(limit) 
