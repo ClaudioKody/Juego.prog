62db796c0206606136999c2cc28597566062890c
Eneles070 <jilenequin133@gmail.com>
2026-08-05 18:54:17 -0300

    archivo helpers creado y codigo

---
+++ utils/helpers.py
@@ -0,0 +1,8 @@
+import  pygame 
+def  load_image ( path ,  scale = 1 ): 
+image  =  pygame . image . load ( path ). convert_alpha () 
+if  scale  !=  1 : 
+size  =  image . get_size () 
+image  =  pygame . transform . scale ( image , ( int ( size [ 0 ]  *  scale ), 
+int ( size [ 1 ]  *  scale ))) 
+return  image
