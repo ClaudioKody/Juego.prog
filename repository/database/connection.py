3d542cfa5620b39222f1c7600bfe43fb64c75233
tomynaveda-design <tomynaveda@gmail.com>
2026-08-07 09:49:39 -0300

    Faltaba el puerto en los datos

--- repository/database/connection.py
+++ repository/database/connection.py
@@ -24,6 +24,7 @@ class MySQLConnection(DatabaseConnection):
         try:
             host = os.getenv("DB_HOST", "localhost")
             user = os.getenv("DB_USER", "root")
+            port = os.getenv("DB_PORT", "3307")
             password = os.getenv("DB_PASSWORD", "")
             database = os.getenv("DB_NAME", "juego_programacion2")
             
